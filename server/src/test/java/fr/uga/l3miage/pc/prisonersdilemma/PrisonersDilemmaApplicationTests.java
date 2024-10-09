392827c7eceb9cd8be9ab14d1d0dd7cf9b1ad563
m1miage-osmanm <Mona.Osman@etu.univ-grenoble-alpes.fr>
2024-10-09 11:14:00 +0200

    added modules, docker, application, and postgres

--- server/src/test/java/fr/uga/l3miage/pc/prisonersdilemma/PrisonersDilemmaApplicationTests.java
+++ server/src/test/java/fr/uga/l3miage/pc/prisonersdilemma/PrisonersDilemmaApplicationTests.java
@@ -2,6 +2,9 @@ package fr.uga.l3miage.pc.prisonersdilemma;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 
 @SpringBootTest
 class PrisonersDilemmaApplicationTests {
