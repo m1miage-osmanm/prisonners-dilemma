3d7faf9da3c0498a8b5da4131a7434a68713796d
m1miage-osmanm <Mona.Osman@etu.univ-grenoble-alpes.fr>
2024-10-16 21:31:46 +0200

    jeu basique fonctionnel, ajout service et classes et un main spring, no test

--- server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/PrisonersDilemmaApplication.java
+++ server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/PrisonersDilemmaApplication.java
@@ -1,10 +1,15 @@
 package fr.uga.l3miage.pc.prisonersdilemma;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrisonersDilemmaApplication {
+public class PrisonersDilemmaApplication implements CommandLineRunner {
+
+	@Autowired
+	private PartieService partieService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrisonersDilemmaApplication.class, args);
@@ -12,4 +17,16 @@ public class PrisonersDilemmaApplication {
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		partieService.initPartie("Alice", "Bob", new DonnantDonnant(),new Trahison(), 5);
+
+		// Jouer tous les tours
+		partieService.jouerTousLesTours();
+
+		// Afficher les résultats de la partie
+		partieService.afficherResultats();
+	}
 }
