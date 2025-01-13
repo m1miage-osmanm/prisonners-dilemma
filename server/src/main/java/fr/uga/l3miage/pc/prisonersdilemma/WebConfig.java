6e20a29964f236ba188ee3678ffce634fcd299f4
m1miage-osmanm <mona.osman@etu.univ-grenoble-alpes.fr>
2025-01-13 13:13:29 +0100

    modif ip vm

--- server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/WebConfig.java
+++ server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200","http://127.0.0.1:4200")
+                .allowedOrigins("http://localhost:4200","http://129.88.210.94:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
