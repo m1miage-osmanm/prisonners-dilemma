425a5664ca131ac1c813a9d16768d2d889403711
m1miage-osmanm <mona.osman@etu.univ-grenoble-alpes.fr>
2025-01-15 16:01:13 +0100

    deploiement front testé

--- server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/WebConfig.java
+++ server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/WebConfig.java
@@ -2,6 +2,7 @@ package fr.uga.l3miage.pc.prisonersdilemma;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,11 +11,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200","http://129.88.210.94:4200")
+                .allowedOrigins("http://localhost:4200","http://129.88.210.94:4200","http://localhost:3000","http://129.88.210.94:5050")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
-
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:[^.]*}")
+                .setViewName("forward:/index.html");
+    }
 }
